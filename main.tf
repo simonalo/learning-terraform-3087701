f87ae9511d4b5fd30363c14a0f482122aab665f0
Chappi <62103748+simonalo@users.noreply.github.com>
2025-10-30 15:31:16 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -43,6 +43,42 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = module.blog_sg.security_group_id
+
+
+  listeners = {
+    ex-http = {
+      port            = 80
+      protocol        = "HTTP"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
