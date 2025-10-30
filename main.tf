7f593eedec5f9d96626b627115ca854dc2789859
Chappi <62103748+simonalo@users.noreply.github.com>
2025-10-30 15:48:06 +0100

    use aws_autoscaling_attachment

--- main.tf
+++ main.tf
@@ -39,11 +39,15 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier  = module.blog_vpc.public_subnets
-  target_group_arns    = module.blog_alb.target_group_arns
   security_groups      = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+}
+
+resource "aws_autoscaling_attachment" "blog_asg_alb" {
+  autoscaling_group_name = module.autoscaling.autoscaling_group_name
+  alb_target_group_arn   = module.blog_alb.target_group_arns[0]
 }
 
 module "blog_alb" {
